bcd3f496c806717bf032a3e7491942d39f3f9b27
Radika2297 <radika2906@gmail.com>
2024-02-10 01:53:09 +0700

    menambahkan fitur baru

--- js/script.js
+++ js/script.js
@@ -11,4 +11,45 @@ document.addEventListener("click", function (e) {
 	if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
 		navbarNav.classList.remove("active");
 	}
+
+	if (!search.contains(e.target) && !searchButton.contains(e.target)) {
+		search.classList.remove("active");
+	}
+
+	if (
+		!shoppingCart.contains(e.target) &&
+		!shoppingCartButton.contains(e.target)
+	) {
+		shoppingCart.classList.remove("active");
+	}
 });
+
+const searchButton = document.querySelector("#search");
+const search = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+searchButton.addEventListener("click", function (e) {
+	search.classList.toggle("active");
+	searchBox.focus();
+	e.preventDefault();
+});
+
+const shoppingCart = document.querySelector(".shopping-cart");
+const shoppingCartButton = document.querySelector("#shopping-cart");
+shoppingCartButton.addEventListener("click", function (e) {
+	shoppingCart.classList.toggle("active");
+	e.preventDefault();
+});
+
+const products = document.querySelector("#products");
+const modal = document.querySelector(".modal");
+products.addEventListener("click", function (e) {
+	e.preventDefault();
+	if (e.target.classList.contains("detail-item-button")) {
+		modal.style.display = "flex";
+	}
+});
+
+document.querySelector(".modal .close-icon").onclick = (e) => {
+	modal.style.display = "none";
+	e.preventDefault();
+};
