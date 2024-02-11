2e16c356c81c3e64daef6ea5623b9403b9f95b3c
Radika2297 <radika2906@gmail.com>
2024-02-11 20:44:45 +0700

    Menambahkan fitur baru ( pengisian keranjang belanja , dan produk dinamis)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 // Toggle Class Active
 
 const navbarNav = document.querySelector(".navbar-nav");
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
 	navbarNav.classList.toggle("active");
+	e.preventDefault();
 };
 
 const hamburger = document.querySelector("#hamburger-menu");
